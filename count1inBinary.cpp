3182f4b8b6a8d534863af28b3f8f1048ee5186ea
xiaeryu <xiaeryu@nus.edu.sg>
2015-05-30 13:34:52 +0800

    Create count1inBinary.cpp

---
+++ count1inBinary.cpp
@@ -0,0 +1,21 @@
+#########################################################
+## LintCode Number 365 Count 1 in Binary
+## Problem description see:
+## http://www.lintcode.com/en/problem/count-1-in-binary/
+#########################################################
+
+class Solution {
+public:
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    int countOnes(int num) {
+        int count = 0;
+        while (num > 0){
+            count += 1;
+            num = num & (num-1);
+        }
+        return count;
+    }
+};
